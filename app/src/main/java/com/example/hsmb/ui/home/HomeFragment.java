e8ef89ac57dbfd14ca0141e0909f77a4880eaad0
Areej <Areej10122@gmail.com>
2021-01-26 21:36:02 +0300

    move between two fragment but not work

--- app/src/main/java/com/example/hsmb/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hsmb/ui/home/HomeFragment.java
@@ -1,28 +1,42 @@
 package com.example.hsmb.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.hsmb.R;
+import com.example.hsmb.ui.gallery.GalleryFragment;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        Button location = root.findViewById(R.id.location);
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new GalleryFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.locatonBooth, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();            }
+        });
 
         return root;
     }
