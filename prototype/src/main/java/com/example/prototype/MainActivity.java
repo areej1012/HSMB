250e31c9eb3e329796fcf8806bd93df9b3211caf
buthina salh <s437012308@st.uqu.edu.sa>
2021-03-26 10:08:11 +0300

    add constraint in all pages in prototype

--- prototype/src/main/java/com/example/prototype/MainActivity.java
+++ prototype/src/main/java/com/example/prototype/MainActivity.java
@@ -20,10 +20,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_main);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
+
 
     }
 }
