56f0d6b3ae27092f31d099712379e45751c0f27d
Areej <areej101223@gmail.com>
2021-04-18 03:15:07 +0300

    update nearst booth

--- app/src/main/java/com/example/hsmb/MapsActivity.java
+++ app/src/main/java/com/example/hsmb/MapsActivity.java
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -26,6 +27,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.maps.android.SphericalUtil;
 
 import java.util.ArrayList;
 
@@ -36,8 +38,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int REQUEST_CODE = 101;
     Location currentLocation;
     LatLng pilgrim = new LatLng(21.413776, 39.886360);
+    Double distance ;
     SupportMapFragment mapFragment;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,39 +51,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-
+        ArrayList<Double> d;
         db.collection("BoothLocation")
                 .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()) {
                     for(QueryDocumentSnapshot document : task.getResult()){
-                        if(document.getString("State").equals("free")){
+
+                       if(document.getString("State").equals("free")){
                             GeoPoint geo = document.getGeoPoint("geo");
                             double lat = geo.getLatitude();
                             double lng = geo.getLongitude();
                             LatLng latLng = new LatLng(lat, lng);
+                           distance  = SphericalUtil.computeDistanceBetween(pilgrim, latLng);
+                             if((distance  / 1000) <= 0.45) {
                             mMap.addMarker(new MarkerOptions().position(latLng).title("Free"));
                             mMap.moveCamera( CameraUpdateFactory.newLatLng(latLng));
                             mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+                            }
+
+
                         }
                         else  if(document.getString("State").equals("busy")){
                             GeoPoint geo = document.getGeoPoint("geo");
                             double lat = geo.getLatitude();
                             double lng = geo.getLongitude();
                             LatLng latLng = new LatLng(lat, lng);
+                           distance  = SphericalUtil.computeDistanceBetween(pilgrim, latLng);
+                             if((distance  / 1000) <= 0.45) {
                             mMap.addMarker(new MarkerOptions().position(latLng).title("Busy").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
                             mMap.moveCamera( CameraUpdateFactory.newLatLng(latLng));
                             mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+                            }
                         }
                         else  if(document.getString("State").equals("sterilization")){
                             GeoPoint geo = document.getGeoPoint("geo");
                             double lat = geo.getLatitude();
                             double lng = geo.getLongitude();
                             LatLng latLng = new LatLng(lat, lng);
+                           distance  = SphericalUtil.computeDistanceBetween(pilgrim, latLng);
+                             if((distance  / 1000) <= 0.45) {
                             mMap.addMarker(new MarkerOptions().position(latLng).title("Sterilization").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
                             mMap.moveCamera( CameraUpdateFactory.newLatLng(latLng));
                             mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+                            }
                         }
                     }
                 }
@@ -105,8 +121,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-
+        mMap.addMarker(new MarkerOptions().position(pilgrim).title("pilgrim").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+        mMap.moveCamera( CameraUpdateFactory.newLatLng(pilgrim));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
 
 
         }
